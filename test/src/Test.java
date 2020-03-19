368ed5e4915d46376ad73af5355f29296cef97c6
geoff_l <geoff.liu@foxmail.com>
2020-03-19 17:20:40 +0800

    second modification.

--- test/src/Test.java
+++ test/src/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		
+		System.out.println("Hello Jeoff, this is a beautiful world, isn't it?");
 	}
 
 }
