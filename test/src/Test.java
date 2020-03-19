ab279cba498c1a7ec3f31d5d3474b75c6a7a75dd
geoff_l <geoff.liu@foxmail.com>
2020-03-20 00:41:37 +0800

    do something before new changes of the dev branche be commit.

--- test/src/Test.java
+++ test/src/Test.java
@@ -6,9 +6,10 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Jeoff, this is a beautiful world, isn't it?"); //abc
-		// HHHMMMHHH
 		System.out.println("Third commit.?");
+		// new lines.
 		System.out.println("modify at dev branch.");
+		
 	}
 
 }
